24a9eda3ce57ba5c075063239bc96149d1245ac0
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-07-25 11:27:47 +0200

    testing

--- up.sh
+++ up.sh
@@ -4,4 +4,4 @@ git commit -m "$*"
 
 git push
 
-chrome "https://www.pythonanywhere.com/user/chrisrestapi/consoles/34860100/"
+open "https://www.pythonanywhere.com/user/chrisrestapi/consoles/34860100/"
