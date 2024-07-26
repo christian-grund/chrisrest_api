8072dcf0ce805ba79ac6b42bda49d7b15c1da38a
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-07-26 11:51:23 +0200

    11 - Funktionen in Serializer nutzen

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.contrib.auth.models import User
 from django.db import models
+from datetime import date
 
 #ForeignKey: wenn auf anderes Model referenziert wird
 class Todo(models.Model):
@@ -8,6 +9,12 @@ class Todo(models.Model):
   description = models.CharField(max_length=255)
   created_at = models.DateField(default=datetime.today)
   user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
+  def time_passed(self):
+    today = date.today()
+    delta = today - self.created_at
+
+    return delta.days
 
   # def user_id(self):
   #   return self.user.id
--- todo/serializers.py
+++ todo/serializers.py
@@ -14,8 +14,9 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
     # user = UserSerializer()
+    # user = CurrentUserDefault()
     user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
 
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'created_at', 'user']   
+        fields = ['id', 'title', 'description', 'created_at', 'user', 'time_passed']   
