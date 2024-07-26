a53f1be12d4c4e15c07246fdeba711c3277db963
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-07-26 13:05:12 +0200

    12 - Create Funktion in Serializer

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework import permissions, viewsets
+from rest_framework import viewsets
+from django.core import serializers
 
-from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
-
-from todo.models import Todo
 from todo.serializers import TodoSerializer
+from todo.models import Todo
 
 
 class TodoViewSet(viewsets.ModelViewSet):
@@ -15,3 +15,11 @@ class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all().order_by('-created_at')
     serializer_class = TodoSerializer
     permission_classes = [] # permissions.IsAuthenticated
+
+    def create(self, request):
+        todo = Todo.objects.create(title= request.POST.get('title', ''), 
+                                  description= request.POST.get('description', ''),
+                                  user= request.user,
+                                )
+        serialized_obj = serializers.serialize('json', [todo, ]) 
+        return HttpResponse(serialized_obj, content_type='application/json')
